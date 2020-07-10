d51ba54dfe6920fc7c8de367a4136d29a3a9a917
Robert Truong <59712697+VinhTruong247@users.noreply.github.com>
2020-07-10 12:09:38 -0400

    Adding mainwork

---
+++ Vinh_Truong_UX, UI, and Microtransactions/js/main.js
@@ -0,0 +1,20 @@
+  
+let iconSet = document.querySelectorAll('.container')
+
+function whenClicked(EventObject) {
+    let clickedIcon = EventObject.currentTarget;
+
+    console.log(`the id of the svg clicked is ${clickedIcon.id}`)
+
+    // console.log(EventObject.currentTarget)
+
+    // console.log(clickedIcon.id)
+
+    // console.log(EventObject)
+
+    clickedIcon.classList.toggle('selected')
+}
+
+iconSet.forEach(function(icon) {
+    icon.addEventListener('click', whenClicked, true)
+})
